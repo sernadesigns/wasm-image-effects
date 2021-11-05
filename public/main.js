a72e9f5695f8c278dc5101cc6f527329e781c9ee
sernadesigns <sernadesigns@gmail.com>
2021-11-05 11:10:55 -0600

    Adds logic in Rust to decode the uploaded file, convert it to grayscale, write it to the PNG format, then return it to JS as an encoded string url for display to the user

--- public/main.js
+++ public/main.js
@@ -18,7 +18,8 @@ async function init() {
     const base64 = fileReader.result.replace(
       /^data:image\/(png|jpeg|jpg);base64,/, ''
     );
-    rustApp.grayscale(base64);
+    let img_data_url = rustApp.grayscale(base64);
+    document.getElementById('new-img').setAttribute('src', img_data_url);
   }
 
   input.addEventListener('change', () => {
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,32 @@
+use base64::{decode, encode};
+use image::load_from_memory;
+use image::ImageOutputFormat::Png;
 use wasm_bindgen::prelude::wasm_bindgen;
 use web_sys::console::log_1 as log;
 
 #[wasm_bindgen]
-pub fn grayscale(encoded_file: &str) {
-  log(&encoded_file.into());
+pub fn grayscale(encoded_file: &str) -> String {
+  log(&"Grayscale called".into());
+
+  // Decode the encoded file
+  let base64_to_vector = decode(encoded_file).unwrap();
+  log(&"Image decoded".into());
+
+  // Load decoded image and store it for manipulation
+  let mut img = load_from_memory(&base64_to_vector).unwrap();
+  log(&"Image loaded".into());
+
+  // Convert the image to grayscale
+  img = img.grayscale();
+  log(&"Grayscale effect applied".into());
+
+  // Write new image to PNG format
+  let mut buffer = vec![];
+  img.write_to(&mut buffer, Png).unwrap();
+  log(&"New image written".into());
+
+  let encoded_img = encode(&buffer);
+  let data_url = format!("data:image/png;base64,{}", encoded_img);
+
+  data_url
 }
